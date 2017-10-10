4181eebe420be5a9a9579db68d509aad11c785df
Renato Frota <renatofrota@gmail.com>
2017-10-10 18:37:28 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # wp-hack-fix
 Script to resolve malware infections on WordPress websites by resintalling all files on core, plugins and themes from their official repositories. Be aware: it removes anything it considers "non-core". Take a backup!
+
+## Dependencies
+
+`wp-cli` is required (available as `wp` command on your shell environment)
+
+## How to use
+
+From the WP root directory (where wp-blog-header.php resides) run:
+
+```
+curl -sO https://raw.githubusercontent.com/renatofrota/wp-hack-fix/master/wp-hack-fix.bash && bash wp-hack-fix.bash
+```
+
+## Donate
+
+Help me keep my stuff Open Source and free.
+
+Think on how much time($) you're saving with this tool and buy me some coffee! :)
+
+> USD
+
+[![Donate](https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=R58RLRMM8YM6U)
+
+> BRL
+
+[![Doar](https://www.paypalobjects.com/pt_BR/i/btn/btn_donate_SM.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=9JMBDY5QA8X5A)
---
+++ wp-hack-fix.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+#############
+# WPHACKFIX #
+#############
+# curl -s https://raw.githubusercontent.com/renatofrota/wp-hack-fix/master/wp-hack-fix.bash && bash wp-hack-fix.bash
+# forcedly reinstall core
+wp core download --force --version=$(wp core version)
+# forcedly remove files that should not exist
+for i in $(wp core verify-checksums 2>&1 | grep 'should not exist:' | cut -d : -f 3-); do rm -v $i; done
+# prevents add_filter and add_action in wp-config.php affecting wp-cli executions
+sed -i 's|^add_filter|if function_exists("add_filter") add_filter|g' wp-config.php
+sed -i 's|^add_action|if function_exists("add_action") add_action|g' wp-config.php
+# forcedly reinstall plugins
+for i in $(wp plugin list --skip-themes --skip-plugins --fields=name | grep -v '^name'); do echo -e "-----\n$i\n-----"; wp plugin install --skip-themes --skip-plugins --force "$i" --version=$(wp plugin list --skip-themes --skip-plugins --name="$i" --fields=version | grep -v '^version'); done
+# forcedly reinstall themes
+for i in $(wp theme list --skip-themes --skip-plugins --fields=name | grep -v '^name'); do echo -e "-----\n$i\n-----"; wp theme install --skip-themes --skip-plugins --force "$i" --version=$(wp theme list --skip-themes --skip-plugins --name="$i" --fields=version | grep -v '^version'); done
+# check
+wp core verify-checksums
+# find nasty include/require on wp-config.php and index.php
+echo -e "\nif you see any file other than 'wp-settings.php' and 'wp-blog-header.php' in the lines below, check your wp-config.php and index.php files for malware code injection:\n"
+grep --color "include\|require" wp-config.php index.php
+echo -e "\nAll done!"
+
+killme() {
+    [[ "$0" == "search-replace-ssl.bash" ]] && echo -n "Done! Self destroying... " && sleep 1 && rm -fv "$0" || echo "It's all done. Do not forget to remove this script.";
+}
+trap killme EXIT
